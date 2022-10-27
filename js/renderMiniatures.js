7992c014848e7ea8c7f49aea61717d0028bec03a
Evgeny Korolev <mts9832899777@gmail.com>
2022-10-27 23:30:09 +0700

    added a loop

--- js/renderMiniatures.js
+++ js/renderMiniatures.js
@@ -8,15 +8,31 @@ const createElements = () => {
   const userPictureComments = userPictureElement.querySelector('.picture__comments');
   const userPictureLikes = userPictureElement.querySelector('.picture__likes');
 
-  userPictureElement.classList.add('random-user--picture');
-  userPictureElement.querySelector('img').src = `./photos/${getValueFromRange(1, 5)}.jpg`;
-  userPictureComments.textContent = getValueFromRange(1, 5);
-  userPictureLikes.textContent = getValueFromRange(1, 5);
-  pictureContainer.append(userPictureElement);
-
+  for (let i = 0; i < 4; i++) {
+    userPictureElement.classList.add('random-user--picture');
+    userPictureElement.querySelector('img').src = `./photos/${getValueFromRange(1, 5)}.jpg`;
+    userPictureComments.textContent = getValueFromRange(1, 5);
+    userPictureLikes.textContent = getValueFromRange(1, 5);
+    pictureContainer.append(userPictureElement);
+  }
 };
 
 export { createElements };
 
 
+// function say() {
+//   console.log(this.nickName + 'says' + this.greeting);
+//   this.goodies.forEach(function (goodie) {
+//     console.log(this.nickName + 'likes' + goodie);
+//   })
+// }
 
+// const cat = {
+//   nickName: 'Keks',
+//   greeting: 'Meow',
+//   goodies: [
+//     'Fresh fish',
+//     'Masters boots'
+//   ],
+//   say
+// };
